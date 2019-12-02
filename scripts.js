10be4953aaa2e27a2905a4171acce07257101182
Jacqueline Remmel and Joel Stockamp <joel@>
2019-12-02 15:43:26 -0800

    Add goodnight moon

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const storyList = {
-    1: ('NEWLINE Wynken, Blynken, and Nod one night NEWLINE Sailed off in a wooden shoe,— NEWLINESailed on a river of crystal light NEWLINE Into a sea of dew NEWLINE "Where are you going, and what do you wish?" NEWLINE The old moon asked the three, NEWLINE"We have come to fish for the herring-fish NEWLINE That live in this beautiful sea; NEWLINE Nets of silver and gold have we," NEWLINE Said Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE The old moon laughed and sang a song, NEWLINE As they rocked in the wooden shoe; NEWLINEAnd the wind that sped them all night long NEWLINE Ruffled the waves of dew; NEWLINEThe little stars were the herring-fish NEWLINE That lived in the beautiful sea NEWLINE"Now cast your nets wherever you wish,— NEWLINE Never afraid are we!" NEWLINE So cried the stars to the fishermen three, NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE All night long their nets they threw NEWLINE To the stars in the twinkling foam,— NEWLINEThen down from the skies came the wooden shoe, NEWLINE Bringing the fishermen home: NEWLINE\'Twas all so pretty a sail, it seemed NEWLINE As if it could not be; NEWLINEAnd some folk thought \'twas a dream they\'d dreamed NEWLINE Of sailing that beautiful sea; NEWLINE But I shall name you the fishermen three: NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE Wynken and Blynken are two little eyes, NEWLINE And Nod is a little head, NEWLINEAnd the wooden shoe that sailed the skies NEWLINE Is a wee one\'s trundle-bed; NEWLINESo shut your eyes while Mother sings NEWLINE Of wonderful sights that be, NEWLINEAnd you shall see the beautiful things NEWLINE As you rock in the misty sea NEWLINE Where the old shoe rocked the fishermen three:— NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod.').split('. '),
-    2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.split('. ')
+    1: ('NEWLINE Wynken, Blynken, and Nod one night NEWLINE Sailed off in a wooden shoe,— NEWLINESailed on a river of crystal light NEWLINE Into a sea of dew NEWLINE "Where are you going, and what do you wish?" NEWLINE The old moon asked the three, NEWLINE"We have come to fish for the herring-fish NEWLINE That live in this beautiful sea; NEWLINE Nets of silver and gold have we," NEWLINE Said Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE The old moon laughed and sang a song, NEWLINE As they rocked in the wooden shoe; NEWLINEAnd the wind that sped them all night long NEWLINE Ruffled the waves of dew; NEWLINEThe little stars were the herring-fish NEWLINE That lived in the beautiful sea NEWLINE"Now cast your nets wherever you wish,— NEWLINE Never afraid are we!" NEWLINE So cried the stars to the fishermen three, NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE All night long their nets they threw NEWLINE To the stars in the twinkling foam,— NEWLINEThen down from the skies came the wooden shoe, NEWLINE Bringing the fishermen home: NEWLINE\'Twas all so pretty a sail, it seemed NEWLINE As if it could not be; NEWLINEAnd some folk thought \'twas a dream they\'d dreamed NEWLINE Of sailing that beautiful sea; NEWLINE But I shall name you the fishermen three: NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod. NEWLINE Wynken and Blynken are two little eyes, NEWLINE And Nod is a little head, NEWLINEAnd the wooden shoe that sailed the skies NEWLINE Is a wee one\'s trundle-bed; NEWLINESo shut your eyes while Mother sings NEWLINE Of wonderful sights that be, NEWLINEAnd you shall see the beautiful things NEWLINE As you rock in the misty sea NEWLINE Where the old shoe rocked the fishermen three: NEWLINE Wynken, NEWLINE Blynken, NEWLINE And Nod.').split('. '),
+    2: 'NEWLINE In the great green room NEWLINE There was a telephone NEWLINE And a red balloon NEWLINE And a picture of NEWLINE The cow jumping over the moon. NEWLINE And there were three little bears sitting on chairs NEWLINE And two little kittens NEWLINE And a pair of mittens NEWLINE And a little toy house NEWLINE And a young mouse. NEWLINE And a comb and a brush and a bowl full of mush NEWLINE And a quiet old lady who was whispering “hush”. NEWLINE Goodnight room NEWLINE Goodnight moon NEWLINE Goodnight cow jumping over the moon. NEWLINE Goodnight light NEWLINE And the red balloon. NEWLINE Goodnight bears NEWLINE Goodnight chairs. NEWLINE Goodnight kittens NEWLINE And goodnight mittens. NEWLINE Goodnight clocks NEWLINE And goodnight socks. NEWLINE Goodnight little house NEWLINE And goodnight mouse. NEWLINE Goodnight comb NEWLINE And goodnight brush NEWLINE Goodnight nobody NEWLINE Goodnight mush NEWLINE And goodnight to the old lady whispering “hush”. NEWLINE Goodnight stars NEWLINE Goodnight air NEWLINE Good night noises everywhere.'.split('. ')
 }
 
 const initialState = {
@@ -13,7 +13,7 @@ const initialState = {
             arrayPosition: 0
         },
         2: {
-            title: 'Lorem Ipsum',
+            title: 'Goodnight Moon',
             storyId: 2,
             storyArray: storyList[2],
             arrayPosition: 0
@@ -69,8 +69,23 @@ const renderSnippet = () => {
     let storyId = store.getState().currentStoryId;
     let currentStory = allStories[storyId];
     const currentSnippet = currentStory.storyArray[currentStory.arrayPosition];
-    //////////////////////////////////////////////////////////////////////////////////////////
     const lineByLine = currentSnippet.split('NEWLINE');
+    if (storyId === 1) {
+        renderWBN(lineByLine);
+    } else {
+        for (let i = 0; i < lineByLine.length; i++) {
+            const renderedLine = document.createTextNode(lineByLine[i]);
+            document.getElementById('story-snippet').insertAdjacentHTML('beforeend', '<br/>');
+            document.getElementById('story-snippet').appendChild(renderedLine);
+        }
+    }
+    document.getElementById('story-snippet').insertAdjacentHTML('beforeend', '<br/>');
+  } else {
+      const selectStoryMessage = document.createTextNode('Select a story to read!');
+      document.getElementById('story-snippet').appendChild(selectStoryMessage);
+  }
+}
+function renderWBN(lineByLine) {
     for (var j = 0; j < lineByLine.length; j++) {
         const renderedLine = document.createTextNode(lineByLine[j]);
         if (j === 2  || j === 4 || j === 6 || j === 8 || j === 9) {
@@ -84,14 +99,7 @@ const renderSnippet = () => {
         }
         document.getElementById('story-snippet').appendChild(renderedLine);
     }
-    document.getElementById('story-snippet').insertAdjacentHTML('beforeend', '<br/>');
-//////////////////////////////////////////////////////////////////////////////////////////
-  } else {
-      const selectStoryMessage = document.createTextNode('Select a story to read!');
-      document.getElementById('story-snippet').appendChild(selectStoryMessage);
-  }
 }
-
 const renderStories = () => {
     const storiesById = store.getState().storiesById;
     for (const storyKey in storiesById) {
@@ -109,6 +117,7 @@ const renderStories = () => {
         document.getElementById('stories').appendChild(li);
     }
 }
+
 
 window.onload = function() {
     renderStories();
@@ -164,7 +173,7 @@ expect(snippetChangeReducer(initialState.storiesById, { type: 'NEXT_SNIPPET', cu
         arrayPosition: 1
     },
     2: {
-        title: 'Lorem Ipsum',
+        title: 'Goodnight Moon',
         storyId: 2,
         storyArray: storyList[2],
         arrayPosition: 0
@@ -179,7 +188,7 @@ expect(snippetChangeReducer(initialState.storiedById, { type: 'RESTART_STORY', c
         arrayPosition: 0
     },
     2: {
-        title: 'Lorem Ipsum',
+        title: 'Goodnight Moon',
         storyId: 2,
         storyArray: storyList[2],
         arrayPosition: 0
@@ -189,8 +198,6 @@ expect(snippetChangeReducer(initialState.storiedById, { type: 'RESTART_STORY', c
 expect(rootReducer(initialState, { type: null })).toEqual(initialState);
 expect(store.getState().currentSongId).toEqual(storyChangeReducer(undefined, { type: null}));
 expect(store.getState().storiesById).toEqual(snippetChangeReducer(undefined, { type: null }));
-
-
 
 
 
